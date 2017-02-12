3c53d86d6cb165027f9634cd658d653cb8fef0e4
Andrea Jemmett <andreajemmett@gmail.com>
2017-02-12 16:50:14 +0100

    log time & Utils & append to file

--- src/main/scala/ircbot/EventsLogger.scala
+++ src/main/scala/ircbot/EventsLogger.scala
@@ -1,23 +1,25 @@
 package ircbot
 
-import java.io.{File, PrintWriter}
-import java.text.SimpleDateFormat
+import java.io.{FileWriter, PrintWriter}
 import java.util.Calendar
 
+import ircbot.Utils._
 
-case class EventsLogger(basePath: String) {
+
+case class EventsLogger(basePath: String, receiver: String) {
 
     private val startedAt = Calendar.getInstance()
 
-    private val file = new File(basePath + "/" + EventsLogger.formatCal(startedAt) + ".log")
-    private val writer = new PrintWriter(file)
+    // The second parameter enables appending instead of overwriting
+    private val fw = new FileWriter(logFilename(basePath, startedAt, receiver), true)
+    private val writer = new PrintWriter(fw)
 
     def apply(event: String): EventsLogger = {
         val logger = if (timeToChangeFile) {
                         writer.close()
-                        EventsLogger(basePath)
+                        EventsLogger(basePath, receiver)
                     } else this
-        logger.writer.append(event + "\n")
+        logger.writer.append(System.currentTimeMillis() + ":" + event + "\n")
         logger.writer.flush()
         logger
     }
@@ -26,12 +28,5 @@ case class EventsLogger(basePath: String) {
 
     private def timeToChangeFile =
         Calendar.getInstance().get(Calendar.DAY_OF_MONTH) != startedAt.get(Calendar.DAY_OF_MONTH)
-
-}
-
-object EventsLogger {
-
-    private val formatter = new SimpleDateFormat("yyyy-MM-dd")
-    private def formatCal(calendar: Calendar) = formatter.format(calendar.getTime)
 
 }
