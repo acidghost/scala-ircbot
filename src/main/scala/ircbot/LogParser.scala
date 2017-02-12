321db155565c627c1e932c83962656b9d774c5c3
Andrea Jemmett <andreajemmett@gmail.com>
2017-02-12 20:34:19 +0100

    handle file not found in LogParser

--- src/main/scala/ircbot/LogParser.scala
+++ src/main/scala/ircbot/LogParser.scala
@@ -6,12 +6,17 @@ import java.util.Date
 import io.circe.parser._
 import ircbot.Utils._
 
+import scala.util.Try
+
 
 object LogParser {
 
     def apply(receiver: String, date: Date, logsPath: String): Map[Date, ServerMessage] = {
         val file = new File(logFilename(logsPath, date, receiver))
-        val reader = new BufferedReader(new FileReader(file))
+        val readerTry = Try(new BufferedReader(new FileReader(file)))
+        if (readerTry.isFailure)
+            return Map.empty[Date, ServerMessage]
+        val reader = readerTry.get
 
         def rebuildLog(messages: Map[Date, ServerMessage]): Map[Date, ServerMessage] = {
             val line = reader.readLine()
