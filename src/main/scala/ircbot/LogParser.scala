004c4842ef4525e8f2f4d00c4f66d032fe54462d
Andrea Jemmett <andreajemmett@gmail.com>
2017-02-12 17:05:42 +0100

    basic logs parser

---
+++ src/main/scala/ircbot/LogParser.scala
@@ -0,0 +1,38 @@
+package ircbot
+
+import java.io.{BufferedReader, File, FileReader}
+import java.util.Date
+
+import io.circe.parser._
+import ircbot.Utils._
+
+
+object LogParser {
+
+    def apply(receiver: String, date: Date, logsPath: String): Map[Date, ServerMessage] = {
+        val file = new File(logFilename(logsPath, date, receiver))
+        val reader = new BufferedReader(new FileReader(file))
+
+        def rebuildLog(messages: Map[Date, ServerMessage]): Map[Date, ServerMessage] = {
+            val line = reader.readLine()
+            if (line == null) {
+                reader.close()
+                return messages
+            }
+
+            val time :: msgStr :: Nil = line.split(":", 2).toList
+            val msgDate = new Date(time.toLong)
+
+            decode[ServerMessage](msgStr) match {
+                case Left(_) =>
+                    // TODO how to notify failure? Just skip for now
+                    rebuildLog(messages)
+                case Right(msg) =>
+                    rebuildLog(messages + (msgDate -> msg))
+            }
+        }
+
+        rebuildLog(Map.empty[Date, ServerMessage])
+    }
+
+}
